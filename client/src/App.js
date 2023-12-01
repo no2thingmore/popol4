a6bfb3b4c3517454c49a6aced023076517144eed
Heo <jyhhg@naver.com>
2023-12-01 13:11:59 +0900

    메뉴 4번째 항목 페이지 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+import History from './components/subway/history';
+import Promise from './components/subway/promise';
+import Apply from './components/subway/apply';
+import StoreSearch from './components/subway/search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/History" element={<History></History>}></Route>
+      <Route path="/Promise" element={<Promise></Promise>}></Route>
+      <Route path="/Apply" element={<Apply></Apply>}></Route>
+      <Route path="/StoreSearch" element={<StoreSearch></StoreSearch>}></Route>
     </div>
   );
 }
---
+++ client/src/components/subway/promise.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Promise(){
+  return(
+    <div id="Promise_Main_Div">
+      <div className="Head_Line">
+        <h2 className="Head_txt">
+          <em>여기에</em>
+          뭔가 글씨가 있겠죠?
+          <span>®</span>
+          </h2>
+        <p>부제</p>
+      </div>
+    </div>
+  )
+}
+
+export default Promise;
