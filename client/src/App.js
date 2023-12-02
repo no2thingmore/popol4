bc2d31009279d176e396a32d94b3d6a807a19f78
GyeongIk <gmldi10009@gmail.com>
2023-12-02 11:22:40 +0900

    오류 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Main from "./components/mainPage";
-import History from "./components/subway/history";
-import Promise from "./components/subway/promise";
-import Apply from "./components/subway/apply";
-import StoreSearch from "./components/subway/search";
-
-=======
 import { Route, Routes } from "react-router-dom";
 import History from './components/subway/history';
 import Promise from './components/subway/promise';
@@ -19,20 +7,13 @@ import StoreSearch from './components/subway/search';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import Main from "./components/mainPage";
-<<<<<<< HEAD
 import Event from './components/NewsPage/Event';
-=======
 import Login from './components/login/login';
->>>>>>> 0e12c99ff81cb2221b807bc62024795d3b1167bd
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Header />
-=======
      <Header />
->>>>>>> 0e12c99ff81cb2221b807bc62024795d3b1167bd
       <Routes>
         <Route path="/History" element={<History></History>}></Route>
         <Route path="/Promise" element={<Promise></Promise>}></Route>
@@ -43,12 +24,8 @@ function App() {
         <Route path="/Promise" element={<Promise></Promise>}></Route>
         <Route path="/Apply" element={<Apply></Apply>}></Route>
         <Route path="/StoreSearch" element={<StoreSearch></StoreSearch>}></Route>
-<<<<<<< HEAD
-        <Route path="/" element={<Main />}></Route>
         <Route path="/Event" element={<Event />}></Route>
-=======
         <Route path='/login' element={<Login />}></Route>
->>>>>>> 0e12c99ff81cb2221b807bc62024795d3b1167bd
       </Routes>
       <Footer />
     </div>
