6b5c49fad2fc734e58257db54e7539dd50be2326
GyeongIk <gmldi10009@gmail.com>
2023-12-02 11:24:14 +0900

    오류 수정

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,6 @@ function App() {
     <div className="App">
      <Header />
       <Routes>
-        <Route path="/History" element={<History></History>}></Route>
-        <Route path="/Promise" element={<Promise></Promise>}></Route>
-        <Route path="/Apply" element={<Apply></Apply>}></Route>
-        <Route path="/StoreSearch" element={<StoreSearch></StoreSearch>}></Route>
         <Route path="/" element={<Main />}></Route>
         <Route path="/History" element={<History></History>}></Route>
         <Route path="/Promise" element={<Promise></Promise>}></Route>
