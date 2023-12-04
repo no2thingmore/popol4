2023d3816e71d19e79b4d07d49e77a0b640e5e6e
hjy <jyhhg@naver.com>
2023-12-04 22:43:46 +0900

    아니 아직 다 안됬다니까?

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -18,75 +18,78 @@ function Header() {
           </ul>
         </div>
       </div>
-      <ul className="menu">
-        <li className="menu-item">
-          메뉴소개
-          <ul className="submenu">
-            <li> 
-              <a href="/">샌드위치</a>
-              <a href="/">랩 · 기타</a>
-              <a href="/">샐러드</a>
-              <a href="/">아침메뉴</a>
-              <a href="/">스마일 썸</a>
-              <a href="/">단체메뉴</a>
-            </li>
-          </ul>
-        </li>
-        <li className="menu-item">
-          이용방법
-          <ul className="submenu">
-            <li>
-              <a href="/">서브웨이 이용방법</a>
-              <a href="/">단체메뉴 이용방법</a>
-              <a href="/">신선한 재료 소개</a>
-              <a href="/">App 이용방법</a>
-            </li>
-          </ul>
-        </li>
-        <li className="menu-item">
-          새소식
-          <ul className="submenu">
-            <li>
-              <a href="/">이벤트 · 프로모션</a>
-              <a href="/">뉴스 · 공지사항</a>
-              <a href="/">광고영상</a>
-            </li>
-          </ul>
-        </li>
-        <li className="menu-item">
-          써브웨이
-          <ul className="submenu">
-            <li>
-              <a href="/">서브웨이 역사</a>
-              <a href="/">단체메뉴 약속</a>
-              <a href="/">샌드위치 아티스트 지원</a>
-              <a href="/">매장찾기</a>
-            </li>
-          </ul>
-        </li>
-        <li className="menu-item">
-          가맹점
-          <ul className="submenu">
-            <li>
-              <a href="/">서브웨이 프랜차이즈</a>
-              <a href="/">가맹관련 FAQ</a>
-              <a href="/">가맹신청 문의</a>
-              <a href="/">지사안내</a>
-              <a href="/">사업설명회</a>
-            </li>
-          </ul>
-        </li>
-        <li className="menu-item">
-          온라인 주문
-          <ul className="submenu">
-            <li>
-              <a href="/">FAST-SUB</a>
-              <a href="/">HOME-SUB</a>
-              <a href="/">단체주문</a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <div className="dropDown">
+        <ul className="menu">
+          <li className="menu-item">
+            메뉴소개
+            <ul className="submenu">
+              <li> 
+                <a href="/">샌드위치</a>
+                <a href="/">랩 · 기타</a>
+                <a href="/">샐러드</a>
+                <a href="/">아침메뉴</a>
+                <a href="/">스마일 썸</a>
+                <a href="/">단체메뉴</a>
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item">
+            이용방법
+            <ul className="submenu">
+              <li>
+                <a href="/">서브웨이 이용방법</a>
+                <a href="/">단체메뉴 이용방법</a>
+                <a href="/">신선한 재료 소개</a>
+                <a href="/">App 이용방법</a>
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item">
+            새소식
+            <ul className="submenu">
+              <li>
+                <a href="/">이벤트 · 프로모션</a>
+                <a href="/">뉴스 · 공지사항</a>
+                <a href="/">광고영상</a>
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item">
+            써브웨이
+            <ul className="submenu">
+              <li>
+                <a href="/history">서브웨이 역사</a>
+                <a href="/promise">단체메뉴 약속</a>
+                <a href="/">샌드위치 아티스트 지원</a>
+              </li>
+                <a href="/">매장찾기</a>
+            </ul>
+          </li>
+          <li className="menu-item">
+            가맹점
+            <ul className="submenu">
+              <li>
+                <a href="/">서브웨이 프랜차이즈</a>
+                <a href="/">가맹관련 FAQ</a>
+                <a href="/">가맹신청 문의</a>
+                <a href="/">지사안내</a>
+                <a href="/">사업설명회</a>
+              </li>
+            </ul>
+          </li>
+          <li className="menu-item">
+            온라인 주문
+            <ul className="submenu">
+              <li>
+                <a href="/">FAST-SUB</a>
+                <a href="/">HOME-SUB</a>
+                <a href="/">단체주문</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
     </div>
   );
 }
--- client/src/components/subway/promise.js
+++ client/src/components/subway/promise.js
@@ -10,67 +10,92 @@ function Promise(){
     <div id="Promise_Main_Div">
       <div className="Head_Line">
         <h2 className="Head_txt">
-          <em>HELLO!</em>
-          We Are LE SSERAFIM
-          {/* <span>®</span> */}
+          <em>SEARCH</em>
+          FOR BETTER
+          <span>®</span>
           </h2>
-        <p id="Head_subtitle">IM FEARLESS</p>
+        <p id="Head_subtitle">우리는 더 좋은 써브웨이를 위해 매일 노력합니다.</p>
       </div>
-      <div id="grid_1st" className="grid_box">
-        <div className="image_sec">
-          <img src={promise1}></img>
-        </div>
-        <div className="grid_txt_area">
-          <h3 className="grid_head_txt">세상은 나를 평가해</h3>
-          <p className="grid_main_txt">
-            The world is my oyster
-          </p>
-        </div>
-
-        <p className="eng">FEARLESS</p>
-      </div>
-      <div id="grid_2nd" className="grid_box">
-        <div className="grid_txt_area">
-          <h3 className="grid_head_txt">ミスや失敗を隠さない</h3>
-          <p className="grid_main_txt">
-            I'm antifragile
-          </p>
-        </div>
-        <div className="image_sec">
-          <img src={promise2}></img>
-        </div>
-        <p className="eng">The Hydra</p>
-
-      </div>
-      <div id="grid_3rd" className="grid_box">
-        <div className="image_sec">
-          <img src={promise3}></img>
-        </div>
-        <div className="grid_txt_area">
-          <h3 className="grid_head_txt">A closed door, a door locked shut, another door slightly ajar I open them all</h3>
-          <p className="grid_main_txt">
-            Burn the bridge Burn it all よく見て、
-            <br />
-            우리는 이 모든 것을 태워 빛이 될 거야
-          </p>
-        </div>
-        <p className="eng">Burn the Bridge</p>
-        
-      </div>
-      <div id="grid_4th" className="grid_box">
-        <div className="grid_txt_area">
-          <h3 className="grid_head_txt">We are unforgiven</h3>
-          <p className="grid_main_txt">
-            We don't have to be forgiven
-            <br />
-            We are unforgiven
-          </p>
-        </div>
-        <div className="image_sec">
-          <img src={promise4}></img>
-        </div>
-        <p className="eng">LE SSERAFIM</p>
-      </div>
+      <ol className="grid_li">
+        <li className="grid_li_item">
+          <div id="grid_1st" className="grid_box">
+            <div className="image_sec">
+              <img src={promise1}></img>
+            </div>
+            <div className="grid_txt_area">
+              <h3 className="grid_head_txt">엄격하게 관리되는 우리의 재료</h3>
+              <p className="grid_main_txt">
+              매일 매장에 배송되는 신선한 야채들은 각 매장에서 정성스럽게 손질됩니다.
+              <br />
+              엄격한 규율에 따라 세척 과정을 거친 야채들은
+              당일 판매되는 양만큼 준비되며 언제나 신선한 최상의 야채를
+              <br />
+              안전하게 제공하는 것이 써브웨이의 목표입니다.
+              </p>
+            </div>
+            <p className="eng_r">Our Veggies</p>
+          </div>
+        </li>
+        <li className="grid_li_item">
+          <div id="grid_2nd" className="grid_box">
+            <div className="grid_txt_area">
+              <h3 className="grid_head_txt">써브웨이만의 특별한 빵</h3>
+              <p className="grid_main_txt">
+                1983년부터 써브웨이는
+                각 매장에서 매일 직접 구워 낸 신선한 샌드위치 빵을 제공합니다.
+                <br />
+                신선한 샌드위치는 신선한 빵에서 시작된다는 철학으로
+                <br />
+                매일 최상의 샌드위치 빵을 제공하기 위해 노력하고 있습니다.
+              </p>
+            </div>
+            <div className="image_sec">
+              <img src={promise2}></img>
+            </div>
+            <p className="eng_l">Our Bread</p>
+          </div>
+        </li>
+        <li className="grid_li_item">
+          <div id="grid_3rd" className="grid_box">
+            <div className="image_sec">
+              <img src={promise3}></img>
+            </div>
+            <div className="grid_txt_area">
+              <h3 className="grid_head_txt">환경을 위한 우리의 노력</h3>
+              <p className="grid_main_txt">
+              써브웨이의 샐러드 보울은 95% 재생 용기로 만들어졌으며,
+              <br />
+              매장 내 일회용품 사용을 줄여 나가고 있습니다.
+              <br />
+              써브웨이는 작은 부분이라도 놓치지 않고
+              <br />
+              환경을 늘 생각하는 브랜드가 되기 위해 지속적으로 노력하고 있습니다.
+              <br />
+              우리는 이 모든 것을 태워 빛이 될 거야
+              </p>
+            </div>
+            <p className="eng_r">Our Impact</p>
+          </div>
+        </li>
+        <li className="grid_li_item">
+          <div id="grid_4th" className="grid_box">
+            <div className="grid_txt_area">
+              <h3 className="grid_head_txt">신선함을 위한 우리의 노력</h3>
+              <p className="grid_main_txt">
+              써브웨이 빵은 인위적 당분이 함유되어 있지 않으며
+              <br />
+              비타민과 칼슘을 강화하고 나트륨을 줄이는 등
+              <br />
+              건강한 먹거리를 제공하기 위해 노력하고 있습니다.
+              </p>
+            </div>
+            <div className="image_sec">
+              <img src={promise4}></img>
+            </div>
+            <p className="eng_r">Our Fresh</p>
+          </div>
+        </li>
+      </ol>
     </div>
   )
 }
