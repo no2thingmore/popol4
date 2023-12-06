b7b04a93338bafff994105d228f2b9f05b302358
GyeongIk <gmldi10009@gmail.com>
2023-12-07 02:12:56 +0900

    오류 수정

---
+++ client/src/components/newsPage/notice.js
@@ -0,0 +1,130 @@
+import "./notice.css";
+
+function notice(){
+  return(
+    <div>
+      <div>
+        <h2> 뉴스ㆍ공지사항 </h2>
+      </div>
+      <div>
+        <div>
+          <p className="??">
+            "총"
+            <strong></strong>
+            "건의 게시글이 있습니다." 
+          </p>
+        </div>
+
+        <div>
+          <tr>
+            <td>
+              <div className=""></div>
+            </td>
+            <td>
+              <div className="">
+                <a href="#" onClick="">
+                  고객 경험 설문조사 프로그램 안내
+                </a>
+              </div>
+            </td>
+            <td>
+              <div className="">날짜</div>
+            </td>
+          </tr>
+        </div>
+
+        <div>
+          <tr>
+            <td>
+              <div className=""></div>
+            </td>
+            <td>
+              <div className="">
+                <a href="#" onClick="">
+                  고객 경험 설문조사 프로그램 안내
+                </a>
+              </div>
+            </td>
+            <td>
+              <div className="">날짜</div>
+            </td>
+          </tr>
+        </div>
+
+        <div>
+          <tr>
+            <td>
+              <div className=""></div>
+            </td>
+            <td>
+              <div className="">
+                <a href="#" onClick="">
+                  고객 경험 설문조사 프로그램 안내
+                </a>
+              </div>
+            </td>
+            <td>
+              <div className="">날짜</div>
+            </td>
+          </tr>
+        </div>
+
+        <div>
+          <tr>
+            <td>
+              <div className=""></div>
+            </td>
+            <td>
+              <div className="">
+                <a href="#" onClick="">
+                  고객 경험 설문조사 프로그램 안내
+                </a>
+              </div>
+            </td>
+            <td>
+              <div className="">날짜</div>
+            </td>
+          </tr>
+        </div>
+
+        <div>
+          <tr>
+            <td>
+              <div className=""></div>
+            </td>
+            <td>
+              <div className="">
+                <a href="#" onClick="">
+                  고객 경험 설문조사 프로그램 안내
+                </a>
+              </div>
+            </td>
+            <td>
+              <div className="">날짜</div>
+            </td>
+          </tr>
+        </div>
+
+        <div>
+          <tr>
+            <td>
+              <div className=""></div>
+            </td>
+            <td>
+              <div className="">
+                <a href="#" onClick="">
+                  고객 경험 설문조사 프로그램 안내
+                </a>
+              </div>
+            </td>
+            <td>
+              <div className="">날짜</div>
+            </td>
+          </tr>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default notice;
