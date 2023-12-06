83a308663187cc707ea6137c859ce7b3a84819a7
jsw <jsw5sw@naver.com>
2023-12-07 00:55:57 +0800

    서브웨이 이용방법 50%

--- client/src/components/howtouse/howtousesubway/howtousesubway.js
+++ client/src/components/howtouse/howtousesubway/howtousesubway.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { Nav } from "react-bootstrap";
-// import "./howtousesubway.css";
+import "./howtousesubway.css";
 import Storeorder from './storeorder';
-import Ordertip from './ordertip';
+import Ordertip from './ordertip_1';
 
 function Howtousesubway(props) {
-  let [select, setSelect] =useState(0);
+  let [select, setSelect] =useState(1);
   return (
     <div className='jsw_maindiv'>
       <div className='jsw_howto'>
@@ -13,17 +13,17 @@ function Howtousesubway(props) {
       </div>
       <div className="inquiry_main">
         
-        <div>
-          <Nav style={{paddingLeft: '2vw'}}variant="tabs" defaultActiveKey="link0">
+        <div id='jsw_container2'>
+          <Nav id='jsw_container_2' variant="tabs" defaultActiveKey="link0">
             <Nav.Item id='jsw_nav1'>
               <Nav.Link className="selcetNav" eventKey="link0" onClick={()=>{
                 setSelect(1)
-              }}>매장에서 주문하기</Nav.Link>
+              }}><p>매장에서 주문하기</p></Nav.Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item id='jsw_nav2'>
               <Nav.Link className="selcetNav" eventKey="link1" onClick={()=>{
                 setSelect(0)
-              }}>주문 TIP</Nav.Link>
+              }}><p>주문 TIP</p></Nav.Link>
             </Nav.Item>
           </Nav>
           <Uicontent selcet={select}></Uicontent>
