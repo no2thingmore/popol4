206cdc87a9c80031cc461e4b3ad8f34dd909a11e
JUNHO <junho2088@gmail.com>
2023-12-06 17:28:37 +0900

    준호의 작품

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -5,32 +5,22 @@ function Footer() {
     <div className="footer">
       <div className="content_jun">
         <div className="util_menu_jun">
-          <ul>
-            <li>
-              <a href="">이용약관</a>
-            </li>
-            <li>
-              <a href="">개인정보처리방침</a>
-            </li>
-            <li>
-              <a href="">써브카드</a>
-            </li>
-            <li>
-              <a href="">점주관리자</a>
-            </li>
-            <li>
-              <a href="">Subway Listens</a>
-            </li>
-          </ul>
+          <button id="jun_b"><a href="/">이용약관</a></button>
+          <button id="jun_b1"><a href="/"><b>개인정보처리방침</b></a></button>
+          <button id="jun_b"><a href="/">써브카드</a></button>
+          <button id="jun_b"><a href="/">점주관리자</a></button>
+          <button id="jun_b"><a href="/">Subway Listens</a></button>
+      
         </div>
-        <div>
-        <span className="addr">서울시 서초구 강남대로 535 프린스빌딩 15층 | </span>
-        <span className="rep">대표 : Cho Kyeong-ik(조경익) | </span>
-        <span className="tel">전화 : 01-234-5678 | </span>
-        <span className="rep">사업자등록번호 : 123-45-6789</span>
-        <p>SUBWAY® is a Registered Trademark of Subway IP LLC. © 2021 Subway IP LLC. All Rights Reserved.</p>
-        {/* <a>인스타그램</a>
-        <a>페이스북</a> */}
+        <div className="line"></div>
+        <div className="footer_text">
+          <span className="addr">&nbsp;&nbsp;&nbsp;&nbsp;서울시 서초구 강남대로 535 프린스빌딩 15층 | </span>
+          <span className="rep">대표 : Cho Kyeong-ik(조경익) | </span>
+          <span className="tel">전화 : 01-234-5678 | </span>
+          <span className="rep">사업자등록번호 : 123-45-6789</span>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;SUBWAY® is a Registered Trademark of Subway IP LLC. © 2021 Subway IP LLC. All Rights Reserved.</p>
+          {/* <a>인스타그램</a>
+          <a>페이스북</a> */}
         </div>
       </div>
     </div>
