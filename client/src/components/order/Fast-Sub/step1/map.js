e292e565d610c3f20c7c7bb1d5c200db4c0d8736
차하민 <hamincha2@gmail.com>
2023-12-07 12:37:25 +0900

    헤더css, map기능 개선

--- client/src/components/order/Fast-Sub/step1/map.js
+++ client/src/components/order/Fast-Sub/step1/map.js
@@ -1,18 +1,13 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const MapContainer = ({ searchPlace }) => {
   const navigate = useNavigate();
 
   useEffect(() => {
     const { kakao } = window;
-  
-    if (
-      kakao &&
-      kakao.maps &&
-      kakao.maps.services &&
-      kakao.maps.services.Places
-    ) {
+
+    if (kakao && kakao.maps && kakao.maps.services && kakao.maps.services.Places) {
       var infowindow = new kakao.maps.InfoWindow({ zIndex: 3 });
       const container = document.getElementById("myMap");
       const options = {
@@ -20,48 +15,57 @@ const MapContainer = ({ searchPlace }) => {
         level: 3,
       };
       const map = new kakao.maps.Map(container, options);
-  
+
       const ps = new kakao.maps.services.Places();
-  
-      // Use category_filter to limit the search to the food category
-      ps.keywordSearch(searchPlace, placesSearchCB, { category_filter: "FD6" });
-  
+
+      // 검색어에 "써브웨이"가 포함되어 있는지 확인
+      if (searchPlace.includes("써브웨이")) {
+        ps.keywordSearch(searchPlace, placesSearchCB, { category_filter: "FD6" });
+      } else {
+        // 포함되어 있지 않으면 "써브웨이" 키워드를 추가하여 검색
+        ps.keywordSearch(`${searchPlace} 써브웨이`, placesSearchCB, { category_filter: "FD6" });
+      }
+
       function placesSearchCB(data, status, pagination) {
         if (status === kakao.maps.services.Status.OK) {
           let bounds = new kakao.maps.LatLngBounds();
-  
+          let subwayFound = false;
+
           for (let i = 0; i < data.length; i++) {
-            // Check if the place category is '음식점' (restaurant)
-            if (data[i].category_name === "음식점") {
+            console.log(data[i].category_name);
+
+            if (data[i].category_name.includes("써브웨이")) {
               displayMarker(data[i]);
               bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+              subwayFound = true;
             }
           }
-  
-          map.setBounds(bounds);
+
+          if (subwayFound) {
+            map.setBounds(bounds);
+          } else {
+            alert("검색 결과에 써브웨이가 없습니다.");
+          }
         }
       }
-  
+
       function displayMarker(place) {
         let marker = new kakao.maps.Marker({
           map: map,
           position: new kakao.maps.LatLng(place.y, place.x),
         });
-  
+
         const location = place.place_name;
         console.log("location: ", location);
-  
-        // 마커에 클릭 이벤트를 등록합니다
+
         kakao.maps.event.addListener(marker, "click", function () {
-          // 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
           infowindow.setContent(
             `<div style="padding:5px;font-size:0.8vw;cursor: pointer;" onclick="handleClick('${location}')">${location}</div>`
           );
           infowindow.open(map, marker);
         });
       }
-  
-      // 클릭 이벤트 핸들러에서 페이지 이동과 함께 location 값을 전달하는 함수
+
       window.handleClick = (location) => {
         navigate(`/order/Fast-Sub/step2/${location}/sandwich/Nan`);
       };
