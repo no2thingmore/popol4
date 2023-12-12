0a76817d95844bcbc24f1f6e6d39ac23f8323b85
tester <test@test.pri>
2023-12-12 17:11:11 +0900

    거의 다 됐어 조금만 기다려

--- client/src/components/subway/apply.js
+++ client/src/components/subway/apply.js
@@ -7,10 +7,10 @@ import slide_img_3 from './art_images/img_sandwich_artist_slider03.jpg';
 import slide_img_4 from './art_images/img_sandwich_artist_slider04.jpg';
 import slide_img_5 from './art_images/img_sandwich_artist_slider05.jpg';
 import important_bg from './art_images/img_sw_artist_point.jpg';
-import important_icon_1 from './art_images/icon_sw_artist_point01.png';
-import important_icon_2 from './art_images/icon_sw_artist_point02.png';
-import important_icon_3 from './art_images/icon_sw_artist_point03.png';
-import important_icon_4 from './art_images/icon_sw_artist_point04.png';
+import icon_1 from './art_images/icon_sw_artist_point01.png';
+import icon_2 from './art_images/icon_sw_artist_point02.png';
+import icon_3 from './art_images/icon_sw_artist_point03.png';
+import icon_4 from './art_images/icon_sw_artist_point04.png';
 import WakDo from './art_images/GgamJjickYee.mp4';
 
 
@@ -202,7 +202,121 @@ function Apply(){
           </div>
         </div>
         {/* 핵심요소 */}
-        <div></div>
+        <div className="Point">
+          <h3>써브웨이 NOW 핵심 요소</h3>
+          <ul className="Point_li">
+            <li className="Point_li_item">
+              <div className="point_icon">
+                <img src={icon_1}></img>
+                <span>Guest First</span>
+              </div>
+            </li>
+            <li className="Point_li_item">
+              <div className="point_icon">
+                <img src={icon_2}></img>
+                <span>Great Food</span>
+              </div>
+            </li>
+            <li className="Point_li_item">
+              <div className="point_icon">
+                <img src={icon_3}></img>
+                <span>Great Store</span>
+              </div>
+            </li>
+            <li className="Point_li_item">
+              <div className="point_icon">
+                <img src={icon_4}></img>
+                <span>Great Team</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* 커리어 요약 */}
+        <div className="career_path">
+          <h3>Career Path</h3>
+          <ol className="path_point">
+            <li>
+							<span class="point_num">01</span>
+							<strong>Sandwich<br />Artist</strong>
+							<div class="view_deep"><p>상세보기</p></div>
+							<div class="layer_view">
+								<p>
+									샌드위치 아티스트™는 고객에게 <br />
+									최고의 경험을 제공하기 위해<br />
+									매장 내 다양한 업무를 실행합니다.
+								</p>
+							</div>
+						</li>
+            <li>
+							<span class="point_num">02</span>
+							<strong>Senior Sandwich<br />Artist</strong>
+							<div class="view_deep"><p>상세보기</p></div>
+							<div class="layer_view">
+								<p>
+									선임 샌드위치 아티스트™는<br />
+									매장 운영 및 관리 업무를<br />
+									습득함으로써 매니저로<br />
+									성장의 기회를 갖게 됩니다.
+								</p>
+							</div>
+						</li>
+            <li>
+							<span class="point_num">03</span>
+							<strong>Shift<br />Manager</strong>
+							<div class="view_deep"><p>상세보기</p></div>
+							<div class="layer_view">
+								<p>
+									시프트 매니저는 함께 일하는<br />
+									샌드위치 아티스트™의 업무를 조율하고<br />
+									매니저의 업무를 지원하는 역할을<br />
+									실행합니다.
+								</p>
+							</div>
+						</li>
+            <li>
+							<span class="point_num">04</span>
+							<strong>Assistant<br />Manager</strong>
+							<div class="view_deep"><p>상세보기</p></div>
+							<div class="layer_view">
+								<p>
+									한 매장을 책임지고 관리하는 매니저가<br />
+									되기 위한 이전 단계로, 매장운영과 관련된<br />
+									중요한 업무에 대해 본격적으로 책임지기<br />
+									시작하는 직급입니다.<br />
+									써브웨이 트레이닝 센터에서 진행되는<br />
+									매니저 교육을 통해 직원관리,<br />
+									매출 및 시스템 관리에 대해 체계적으로<br />
+									배울 수 있는 기회가 제공됩니다.
+								</p>
+							</div>
+						</li>
+            <li>
+							<span class="point_num">05</span>
+							<strong>Manager</strong>
+							<div class="view_deep"><p>상세보기</p></div>
+							<div class="layer_view">
+								<p>
+									매장 운영 및 관리의 책임자로서<br />
+									역할을 실행하며 온라인 학습 과정을 통해<br />
+									지속적으로 업무능력 향상의 기회가<br />
+									제공됩니다.
+								</p>
+							</div>
+						</li>
+            <li>
+							<span class="point_num">06</span>
+							<strong>Multi-Unit<br />Manager</strong>
+							<div class="view_deep"><p>상세보기</p></div>
+							<div class="layer_view">
+								<p>
+									한 개 이상의 매장을<br />
+									운영 및 관리하는<br />
+									매니저입니다.
+								</p>
+							</div>
+						</li>
+          </ol>
+        </div>
         {/* 채용지원 */}
         <div></div>
       </div>
