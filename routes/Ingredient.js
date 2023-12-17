a2f53e9c925515f40a8d5e720c70c0fe90bbff9f
IkGy <138839229+IkGy@users.noreply.github.com>
2023-12-17 17:21:21 +0900

    수정

--- routes/Ingredient.js
+++ routes/Ingredient.js
@@ -14,8 +14,6 @@ router
     })
 })
 
-// 가나다
-
 .get('/ingreDient', (req,res,next)=>{
     Ingredient.findAll()
     .then((result)=>{
@@ -25,5 +23,4 @@ router
       res.status(500).send(err)
     })
 })
-//test
 module.exports = router;
