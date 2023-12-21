f964cefa3913227ee62cb083c6f710fd3ac9ac3f
YUJUN <iiww123@naver.com>
2023-12-21 11:39:12 +0900

    새소식 공지사항 디테일 완성

--- client/src/components/newsPage/noticedetail.js
+++ client/src/components/newsPage/noticedetail.js
@@ -1,7 +1,7 @@
 import "./noticedetail.css";
 import React from "react";
 
-// import d_1 from "./image/notice_1";
+import d1 from "./image/notice_1.jpg";
 import { useParams } from "react-router-dom";
 
 function Noticedetail(){
@@ -14,7 +14,7 @@ console.log(id);
                 <p>2023.08.29 14:55</p>
             </div>
             <div className="n_1_image">
-                {/* <img src={d_1}></img> */}
+                <img src={d1}></img>
                 <p>써브웨이와 함께하는 JTBC 수목드라마 '이 연애는 불가항력' 많은 시청 부탁 드립니다.</p>
             </div>
             <div className="n_back_b">
