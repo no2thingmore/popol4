3b31aa440c04a316dfffd2ec03cd0b768ed058b3
차하민 <hamincha2@gmail.com>
2024-02-03 16:56:20 +0900

    gg

--- client/src/components/franchise/branchinformation/branchinformation.js
+++ client/src/components/franchise/branchinformation/branchinformation.js
@@ -17,11 +17,11 @@ function BranchInformation() {
     setTimeout(() => {
       setEnd("end");
     }, 100);
-    
+
     localStorage.setItem("selectedMenu", menu);
   }, [menu]);
 
-  <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
+  <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>;
 
   return (
     <div className="branchinformation">
@@ -52,14 +52,19 @@ function BranchInformation() {
           {menu === "서울" && (
             <div className={"start " + end}>
               <div className="jh_seoul">
+                <ul id="seoul_label">
+                  <li>
+                    <span id="seoul_label1">● </span>서울강북지사
+                  </li>
+                  <li>
+                    <span id="seoul_label2">● </span>서울강남지사
+                  </li>
+                </ul>
                 <div className="seoul_box">
                   <div className="seoul_img">
-                    <img className="seoul_img1" src={img1}/>
+                    <img className="seoul_img1" src={img1} />
                   </div>
-                  <ul id="seoul_label">
-                    <li><span id="seoul_label1">● </span>서울강북지사</li>
-                    <li><span id="seoul_label2">● </span>서울강남지사</li>
-                  </ul>
+
                   <div className="seoul_text">
                     <div className="seoul_text1">
                       <h3>서울강북지사</h3>
@@ -75,8 +80,11 @@ function BranchInformation() {
                       <p>팩스번호 : 02-6356-7907</p>
                     </div>
                     <div id="seoul_a">
-                    <a href="/franchise/subwayfranchise">가맹요건 알아보기 <i class="fa-solid fa-arrow-right"></i></a>
-                  </div>
+                      <a href="/franchise/subwayfranchise">
+                        가맹요건 알아보기{" "}
+                        <i class="fa-solid fa-arrow-right"></i>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -87,15 +95,22 @@ function BranchInformation() {
             <div className={"start " + end}>
               <div>
                 <div className="jh_center">
+                  <ul id="seoul_label">
+                    <li>
+                      <span id="center_label1">● </span>경기서부인천지사
+                    </li>
+                    <li>
+                      <span id="center_label2">● </span>경기남부지사
+                    </li>
+                    <li>
+                      <span id="center_label3">● </span>경기북강원지사
+                    </li>
+                  </ul>
                   <div className="seoul_box">
                     <div className="seoul_img">
-                      <img className="seoul_img1" src={img2}/>
+                      <img className="seoul_img1" src={img2} />
                     </div>
-                    <ul id="seoul_label">
-                      <li><span id="center_label1">● </span>경기서부인천지사</li>
-                      <li><span id="center_label2">● </span>경기남부지사</li>
-                      <li><span id="center_label3">● </span>경기북강원지사</li>
-                    </ul>
+
                     <div className="center_text">
                       <div className="seoul_text1">
                         <h3>경기서부인천지사</h3>
@@ -112,12 +127,18 @@ function BranchInformation() {
                       <div className="seoul_text1">
                         <h3>경기북강원지사</h3>
                         <b>김주홍 지사장</b>
-                        <p>경기도 하남시 미사대로 510 한강미사이아이에스비즈타워 4층 423호</p>
+                        <p>
+                          경기도 하남시 미사대로 510 한강미사이아이에스비즈타워
+                          4층 423호
+                        </p>
                         <p>연락처 : 031-877-1146</p>
                         <p>팩스번호 : 0504-384-1146</p>
                       </div>
                       <div id="seoul_a">
-                        <a href="/franchise/subwayfranchise">가맹요건 알아보기 <i class="fa-solid fa-arrow-right"></i></a>
+                        <a href="/franchise/subwayfranchise">
+                          가맹요건 알아보기{" "}
+                          <i class="fa-solid fa-arrow-right"></i>
+                        </a>
                       </div>
                     </div>
                   </div>
@@ -129,23 +150,39 @@ function BranchInformation() {
             <div className={"start " + end}>
               <div>
                 <div className="jh_country">
+                <ul id="seoul_label">
+                      <li>
+                        <span id="country_label1">● </span>대전충청지사
+                      </li>
+                      <li>
+                        <span id="country_label2">● </span>대구경북지사
+                      </li>
+                      <li>
+                        <span id="country_label3">● </span>울산경남지사
+                      </li>
+                      <li>
+                        <span id="country_label4">● </span>부산김해지사
+                      </li>
+                      <li>
+                        <span id="country_label5">● </span>광주전라지사
+                      </li>
+                      <li>
+                        <span id="country_label6">● </span>경기북강원지사
+                      </li>
+                    </ul>
                   <div className="seoul_box">
                     <div className="seoul_img">
-                      <img className="jh_country_img" src={img3}/>
+                      <img className="jh_country_img" src={img3} />
                     </div>
-                    <ul id="seoul_label">
-                      <li><span id="country_label1">● </span>대전충청지사</li>
-                      <li><span id="country_label2">● </span>대구경북지사</li>
-                      <li><span id="country_label3">● </span>울산경남지사</li>
-                      <li><span id="country_label4">● </span>부산김해지사</li>
-                      <li><span id="country_label5">● </span>광주전라지사</li>
-                      <li><span id="country_label6">● </span>경기북강원지사</li>
-                    </ul>
+                    
                     <div className="country_text">
                       <div className="seoul_text1">
                         <h3>대전충청지사</h3>
                         <b>변정원 지사장</b>
-                        <p>대전광역시 서구 둔산대로 117번길 95, 리더스타운 C동 501호</p>
+                        <p>
+                          대전광역시 서구 둔산대로 117번길 95, 리더스타운 C동
+                          501호
+                        </p>
                         <p>연락처 : 042-321-1151</p>
                         <p>팩스번호 : 042-321-0118</p>
                       </div>
@@ -166,7 +203,10 @@ function BranchInformation() {
                       <div className="seoul_text1">
                         <h3>부산김해지사</h3>
                         <b>이한종 지사장</b>
-                        <p>부산시 해운대구 센텀중앙로 48 에이스 하이테크 21 1710호</p>
+                        <p>
+                          부산시 해운대구 센텀중앙로 48 에이스 하이테크 21
+                          1710호
+                        </p>
                         <p>연락처 : 051-741-1124</p>
                         <p>팩스번호 : 없음</p>
                       </div>
@@ -174,7 +214,10 @@ function BranchInformation() {
                       <div className="seoul_text1">
                         <h3>광주전라지사</h3>
                         <b>함경민 지사장</b>
-                        <p>광주광역시 서구 시청로 92,(유탑 유블레스 트윈시티), 202호</p>
+                        <p>
+                          광주광역시 서구 시청로 92,(유탑 유블레스 트윈시티),
+                          202호
+                        </p>
                         <p>연락처 : 051-6462-3900</p>
                         <p>팩스번호 : 062-385-1121</p>
                       </div>
@@ -182,12 +225,18 @@ function BranchInformation() {
                       <div className="seoul_text1">
                         <h3>경기북강원지사</h3>
                         <b>김주홍 지사장</b>
-                        <p>경기도 하남시 미사대로 510 한강미사아이에스비즈타워 4층 423호</p>
+                        <p>
+                          경기도 하남시 미사대로 510 한강미사아이에스비즈타워
+                          4층 423호
+                        </p>
                         <p>연락처 : 031-877-1146</p>
                         <p>팩스번호 : 0504-384-1146</p>
                       </div>
                       <div id="seoul_a">
-                        <a href="/franchise/subwayfranchise">가맹요건 알아보기 <i class="fa-solid fa-arrow-right"></i></a>
+                        <a href="/franchise/subwayfranchise">
+                          가맹요건 알아보기{" "}
+                          <i class="fa-solid fa-arrow-right"></i>
+                        </a>
                       </div>
                     </div>
                   </div>
